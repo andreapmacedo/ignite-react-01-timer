09efd1a7b0f8d84a9bc2a772cbbbd18889fd147e
andreapmacedo <aa.andremacedo@gmail.com>
2023-08-27 18:03:16 -0300

    adiciona aula 04, módulo 05

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { defaultTheme } from "./themes/default"
 import { GlobalStyle } from "./styles.ts/global"
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
